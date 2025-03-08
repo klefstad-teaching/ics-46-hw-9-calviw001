93398c8114c337e8df7dde82364d76554cf8be47
calviw001 <calviw8@uci.edu>
2025-03-08 09:03:39 +0000

    Created the edit_distance_within() function and the is_adjacent() function.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -7,12 +7,36 @@ void error(string word1, string word2, string msg) {
 
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
-    return false;
+    int length1 = str1.size() + 1;
+    int length2 = str2.size() + 1;
+    int MED[length1][length2];
+
+    for (int i = 0; i < length1; ++i) {
+        MED[i][0] = i;
+    }
+    for (int j = 0; j < length2; ++j) {
+        MED[0][j] = j;
+    }
+
+    for (int i = 1; i < length1; ++i) {
+        for (int j = 1; j < length2; ++j) {
+            if (str1[i-1] == str2[j-1]) {
+                MED[i][j] = MED[i-1][j-1];
+            } else {
+                MED[i][j] = 1 + min({MED[i][j-1], MED[i-1][j-1], MED[i-1][j]});
+            }
+        }
+    }
+
+    return MED[str1.size()][str2.size()] <= d;
 }
 
 
 bool is_adjacent(const string& word1, const string& word2) {
-    return false;
+    if (word1 == word2) {
+        return true;
+    }
+    return edit_distance_within(word1, word2, 1);
 }
 
 
