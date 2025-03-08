7c94a8f9015b7b95cbe534e75c596d4e04213324
calviw001 <calviw8@uci.edu>
2025-03-08 07:41:43 +0000

    Created a gtests for load_words() using a new file shortwords.txt.

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,22 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+namespace {
+
+  TEST(Ladder, Load_Words) {
+    set<string> word_list;
+    vector<string> expected_words = {"cog", "dog", "dot", "hot", "log", "lot"};
+    string file_name = "src/shortwords.txt";
+    load_words(word_list, file_name);
+    EXPECT_TRUE(word_list.size() == 6);
+    for (const string& word : expected_words) {
+        EXPECT_FALSE(word_list.find(word) == word_list.end());
+    }
+  }
+
+
+  int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+  }
 }
