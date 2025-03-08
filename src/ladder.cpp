7f90fa12a84acd70fe271d3c158831c3df406b99
calviw001 <calviw8@uci.edu>
2025-03-08 09:47:27 +0000

    Created the print_word_ladder() function.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -99,7 +99,10 @@ void load_words(set<string> & word_list, const string& file_name) {
 
 
 void print_word_ladder(const vector<string>& ladder) {
-
+    for (auto word : ladder) {
+        cout << word << " ";
+    }
+    cout << endl;
 }
 
 
