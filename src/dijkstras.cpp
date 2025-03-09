3e273bea61241fe719642d7d7fb1b73a03a8e965
calviw001 <calviw8@uci.edu>
2025-03-09 04:37:29 +0000

    Created the print_path() function.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -57,5 +57,9 @@ vector<int> extract_shortest_path(const vector<int>& previous, int destination) 
 
 
 void print_path(const vector<int>& v, int total) {
-
+    for (size_t i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << "Total cost is: " << total << endl;
 }
