075eae80c3e7cba23da066df7301149229759d43
calviw001 <calviw8@uci.edu>
2025-03-09 03:43:07 +0000

    Attempted the dijkstra_shortest_path() function.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -2,8 +2,39 @@
 
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-    vector<int> result;
-    return result;
+    int numVertices = G.size();
+    vector<int> distances(numVertices, INF);
+    vector<bool> visited(numVertices, false);
+    // Set distance from source to source to 0
+    distances[source] = 0;
+    // Set all vertices to unexplored
+    previous.assign(numVertices, -1); 
+
+    priority_queue<pair<int,int>> minHeap; // pair<vertex, weight>
+    minHeap.push({source, 0});
+
+    while (!minHeap.empty()) {
+        int u = minHeap.top().first;
+        int dest_u =  minHeap.top().second;
+        minHeap.pop();
+
+        if (visited[u]) {
+            continue;
+        }
+        visited[u] = true;
+
+        for (Edge edge : G[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
+            
+            if (!visited[v] && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                minHeap.push({v, distances[v]});
+            }
+        }
+    }
+    return distances;
 }
 
 
